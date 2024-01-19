fbeb8ee6b164172aeeb5ce7a9777db196ff73016
Waylon Dixon <wayjay4@yahoo.com>
2024-01-19 11:46:53 -0700

    removed unused state management properties

--- resources/js/Pages/Todo/Stores/TodoStore.js
+++ resources/js/Pages/Todo/Stores/TodoStore.js
@@ -3,18 +3,11 @@ import create from 'zustand';
 const useTodoStore = create((set, get) => ({
     name: (localStorage.getItem('name')) ? JSON.parse(localStorage.getItem('name')) : '',
     saveNameInLocalStorage: (name) => localStorage.setItem('name', JSON.stringify(name)),
-    handleNameInput: (event) => set(() => ({
-        name: event.target.value,
-    })),
-    todos: (localStorage.getItem('todos')) ? JSON.parse(localStorage.getItem('todos')) : [],
+    handleNameInput: (event) => set(() => ({name: event.target.value.trim()})),
+    todos: [],
     setTodos: (todos) => {
-        const idForTodo = todos.slice(-1).pop()['id']+1;
-        set(state => ({todos: todos, idForTodo: idForTodo}));
-        get().saveIdForTodoInLocalStorage(idForTodo);
+        set(state => ({todos: todos}));
     },
-    saveTodosInLocalStorage: (todos) => localStorage.setItem('todos', JSON.stringify(todos)),
-    idForTodo: (localStorage.getItem('idForTodo')) ? JSON.parse(localStorage.getItem('idForTodo')) : 1,
-    saveIdForTodoInLocalStorage: (idForTodo) => localStorage.setItem('idForTodo', JSON.stringify(idForTodo)),
     todosFiltered: (filter) => {
         if(filter === 'all') {
             return get().todos;
@@ -26,77 +19,6 @@ const useTodoStore = create((set, get) => ({
             return get().todos.filter(todo => todo.isComplete);
         }
     },
-    addTodo: (todoTitle) => {
-        set(state => ({
-            todos: [
-                ...state.todos,
-                {
-                    id: state.idForTodo,
-                    title: todoTitle,
-                    isComplete: false,
-                    isEditing: false,
-                }
-            ],
-            idForTodo: state.idForTodo +1,
-        }));
-
-        get().saveIdForTodoInLocalStorage(get().idForTodo);
-    },
-    clearCompleted: () => set(state => ({
-        todos: [...state.todos].filter(todo => !todo.isComplete),
-    })),
-    deleteTodo: (id) => set(state => ({
-        todos: [...state.todos].filter((todo) => todo.id !== id),
-    })),
-    completeTodo: (id) => set(state => ({
-        todos: state.todos.map(todo => {
-            if(todo.id === id) {
-                todo.isComplete = !todo.isComplete;
-            }
-
-            return todo;
-        }),
-    })),
-    markAsEditing: (id) => set(state => ({
-        todos: state.todos.map(todo => {
-            if(todo.id === id) {
-                todo.isEditing = true;
-            }
-
-            return todo;
-        }),
-    })),
-    cancelEdit: (id) => set(state => ({
-        todos: state.todos.map(todo => {
-            if(todo.id === id) {
-                todo.isEditing = false;
-            }
-
-            return todo;
-        }),
-    })),
-    completeAllTodos: () => set(state => ({
-        todos: state.todos.map(todo => {
-            todo.isComplete = true;
-
-            return todo;
-        })
-    })),
-    updateTodo: (event, id) => set(state => ({
-        todos: state.todos.map(todo => {
-            if(todo.id === id) {
-                if(event.target.value.trim().length === 0) {
-                    todo.isEditing = false;
-                    return todo;
-                }
-
-                todo.title = event.target.value;
-                todo.isEditing = false;
-            }
-
-            return todo;
-        }),
-    })),
     remaining: () => get().todos.filter(todo => !todo.isComplete).length,
 }));
 
