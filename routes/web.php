8671d46ca1d64494e7e8a42fc520391db811a06b
Waylon Dixon <wayjay4@yahoo.com>
2024-01-19 12:53:51 -0700

    modified readme for installation instructions, .env.example, and web routes for home

--- routes/web.php
+++ routes/web.php
@@ -16,31 +16,10 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/todoapp', [\App\Http\Controllers\TodoController::class, 'index'])->name('todos.index');
+Route::get('/', [\App\Http\Controllers\TodoController::class, 'index'])->name('todos.index');
 Route::post('/todos', [\App\Http\Controllers\TodoController::class, 'store'])->name('todos.store');
 Route::post('/todos', [\App\Http\Controllers\TodoController::class, 'store'])->name('todos.store');
 Route::delete('/todo/{todo}', [\App\Http\Controllers\TodoController::class, 'destroy'])->name('todo.destroy');
 Route::patch('/todo/{todo}', [\App\Http\Controllers\TodoController::class, 'update'])->name('todo.update');
 Route::patch('/todos/completeAllTodos', [\App\Http\Controllers\TodoController::class, 'completeAllTodos'])->name('todos.completeAllTodos');
 Route::delete('/todos/clearCompletedTodos', [\App\Http\Controllers\TodoController::class, 'clearCompletedTodos'])->name('todos.clearCompletedTodos');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
