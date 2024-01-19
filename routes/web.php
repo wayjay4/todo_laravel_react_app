6ac914c0318e87a0ec2a7b81f48615eadb3a2261
Waylon Dixon <wayjay4@yahoo.com>
2024-01-19 14:44:45 -0700

    updated route for home

--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,20 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+//Route::get('/', function () {
+//    return Inertia::render('Welcome', [
+//        'canLogin' => Route::has('login'),
+//        'canRegister' => Route::has('register'),
+//        'laravelVersion' => Application::VERSION,
+//        'phpVersion' => PHP_VERSION,
+//    ]);
+//});
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/todoapp', function () {
+Route::get('/', function () {
     return Inertia::render('Todo/TodoApp');
 })->name('todo_app');
 
