fb94779c460bd1fc273e9b8db01ea05ad77ff7fb
Waylon Dixon <wayjay4@yahoo.com>
2024-04-07 13:37:29 -0700

    ran composer update and npm update, also fixed unresolved promise in TodoStore.js

--- resources/js/Pages/Todo/Stores/TodoStore.js
+++ resources/js/Pages/Todo/Stores/TodoStore.js
@@ -1,4 +1,4 @@
-import create from 'zustand';
+import { create } from 'zustand'
 
 const useTodoStore = create((set, get) => ({
     name: (localStorage.getItem('name')) ? JSON.parse(localStorage.getItem('name')) : '',
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -18,4 +18,6 @@ createInertiaApp({
     progress: {
         color: '#4B5563',
     },
+}).then(()=>{
+    // do nothing
 });
